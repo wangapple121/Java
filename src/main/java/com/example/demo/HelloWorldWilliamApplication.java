c25fe091a3f8f9cb40181aa7e69e18ea43f9c01c
william <wangliyang2005@163.com>
2020-05-24 16:24:00 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/HelloWorldWilliamApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class HelloWorldWilliamApplication {
+    @RequestMapping("/")
+    public String HelloWorldWilliamApplication(){
+        return "william test";
+    }
+
+    @RequestMapping("/hello")
+    public String Hello(){
+        return "Hello world";
+    }
+
+}
